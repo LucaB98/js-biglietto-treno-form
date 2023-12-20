ff9e23812d355dfc4b95722b1706bb7b4629b713
LucaB98 <lucabellan98@gmail.com>
2023-12-20 23:37:55 +0100

    finish ticket

--- js/script.js
+++ js/script.js
@@ -26,34 +26,71 @@ console.log('JS OK')
 const nameField = document.getElementById('name');
 const ageField = document.getElementById('age');
 const kmsField = document.getElementById('kms');
-console.log(nameField, ageField, kmsField)
+console.log(nameField, ageField, kmsField);
 // BUTTONS
 
-const resetButton = document.getElementById('reset-button')
-const confirmButton = document.getElementById('confirm-button')
-const buyButton = document.getElementById('buy-button')
-console.log(resetButton, confirmButton, buyButton)
+const resetButton = document.getElementById('reset-button');
+const confirmButton = document.getElementById('confirm-button');
+const buyButton = document.getElementById('buy-button');
+console.log(resetButton, confirmButton, buyButton);
 
 // TICKET
 
-const ticketSection = document.getElementById('ticket-section')
-const passangerElement = document.getElementById('passanger-name')
-const rateElement = document.getElementById('rate')
-const carElement = document.getElementById('car')
-const pnrElement = document.getElementById('pnr')
-const priceElement = document.getElementById('price')
-console.log(ticketSection, passangerElement, rateElement, carElement, pnrElement, priceElement)
+const ticketSection = document.getElementById('ticket-section');
+const passangerElement = document.getElementById('passenger-name');
+const rateElement = document.getElementById('rate');
+const carElement = document.getElementById('car');
+const pnrElement = document.getElementById('pnr');
+const priceElement = document.getElementById('price');
+console.log(ticketSection, passangerElement, rateElement, carElement, pnrElement, priceElement);
 // VARIABILI INIZIALI
 
-const pricePerKms = 0.21
-let rateName = 'Tariffa Ordinaria'
-console.log(pricePerKms)
+const pricePerKms = 0.21;
+let rateName = 'Tariffa Ordinaria';
+console.log(pricePerKms);
 // EVENT LISTENER
 
 confirmButton.addEventListener('click', function(){
     // recupero i valori del form
-    const nameValue = nameField.value.trim()
-    const kmsValue = parseInt(kmsField.value)
-    const ageValue = ageField.value
-    console.log(nameValue, kmsValue, ageValue)
+    const nameValue = nameField.value.trim();
+    const kmsValue = parseInt(kmsField.value);
+    const ageValue = ageField.value;
+    console.log(nameValue, kmsValue, ageValue);
+
+
+    // CALCOLO IL PREZZO BASE
+
+    let price = pricePerKms * kmsValue;
+
+    // CALCOLO EVENTUALE SCONTO
+
+    if(ageValue === 'under'){
+        rateName =  'Tariffa Minorenni';
+        price = price * 0.8;
+    } else if (ageValue === 'over'){
+        rateName = 'Tariffa Over 65';
+        price = price * 0.6;
+    }
+    console.log(price, rateName)
+
+    // RAMDOMIZZO NUMERO CARROZZA
+
+    const car = Math.floor(Math.random() * 10) + 1;
+    
+    // RAMDOMIZZO NUMERO BIGLIETTO
+
+    const pnr = Math.floor(Math.random() * (100000 - 90000)) + 90000;
+    console.log(car, pnr);
+
+    // MONTIAMO I DATI NEL BIGLIETTO
+
+    passangerElement.innerText = nameValue;
+    rateElement.innerText = rateName;
+    carElement.innerText = car;
+    pnrElement.innerText = pnr;
+    priceElement.innerText = '€' + price.toFixed(2);
+    console.log(passangerElement, rateElement, carElement, pnrElement, priceElement);
+    // mostraimo la section
+
+    ticketSection.classList.remove('d-none');
 })
